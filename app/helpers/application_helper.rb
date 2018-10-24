f2d2e3b23e9342263b9ff63b7a63b77ed2d89311
Sam E <sammy@Sams-MacBook-Pro-2.local>
2018-10-24 15:54:53 -0500

    sorting helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def sort_people(event, people)
+    people.sort { |a,b| a.up_by(event) && b.up_by(event) ? a.up_by(event) <=> b.up_by(event) : a.up_by(event) ? -1 : 1 }
+  end
 end
